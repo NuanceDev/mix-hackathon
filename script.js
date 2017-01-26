c0298973507d0477f78bc4252c1ff87f9db0d4cd
William Mura <william.mura@nuance.com>
2017-01-26 14:55:46 -0500

    Request client side

--- script.js
+++ script.js
@@ -1,6 +1,56 @@
-var iframe = document.createElement("iframe");
-iframe.src = "http://199.245.57.4/";
+var config = {
+    slackUrl : '',
+    slackToken: '',
+    community: ''
+};
 
 $(document).ready(function() {
-    $('#slack-invitation-iframe').append(iframe);
+    $('.submit').click(function() {
+        var email = $('.field').val();
+        $('.submit').prop('disabled', true);
+        $.ajax({
+            type: 'POST',
+            url: 'https://'+ config.slackUrl + '/api/users.admin.invite',
+            data: {
+                email: email,
+                token: config.slackToken,
+                set_active: true
+            },
+            complete(result, status) {
+                var body = result.responseJSON;
+                if (body.ok) {
+                    $('.result').html(
+                        'Success! Check &ldquo;'+ email +'&rdquo; for an invite from Slack.'
+                    )
+                } else {
+                    var error = body.error
+                    if (error === 'already_invited' || error === 'already_in_team') {
+                        $('.result').html(
+                            'Success! You were already invited.<br>' +
+                            'Visit <a target="_blank" href="https://'+ config.slackUrl +'">'+ config.community +'</a>'
+                        )
+                        $('.submit').prop('disabled', false);
+                        $('.field').val('');
+                        return;
+                    } else if (error === 'invalid_email') {
+                        error = 'The email you entered is an invalid email.';
+                    } else if (error === 'invalid_auth') {
+                        error = 'Something has gone wrong. Please contact a system administrator.';
+                    }
+                    $('.result').text('Failed! ' + error);
+                }
+
+                // Reset field
+                $('.submit').prop('disabled', false);
+                $('.field').val('');
+            }
+        });
+
+    });
+
+    var send = send;
+
+
+
+
 });
